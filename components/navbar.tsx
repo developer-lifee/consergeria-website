b52e64921a768eb6c82275e0d06bedd84a5aee79
developerlifee <eavila@uniempresarial.edu.co>
2026-08-27 17:54:10 -0500

    feat: wrap navbar logo in link tag for navigation

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -33,6 +33,7 @@ export function Navbar() {
       <div className="max-w-7xl mx-auto px-6 lg:px-8">
         <div className="flex items-center justify-between h-18 lg:h-20">
           {/* Logo */}
+          <a href="#" className="flex items-center gap-3">
             <div className="w-10 h-10 flex items-center justify-center relative bg-indigo-900/40 rounded-lg border border-indigo-500/30 overflow-hidden">
               <img
                 src="logo.png"
